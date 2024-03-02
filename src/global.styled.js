e3fc9a93dd85d0f451350e292bae826c1e206496
Yago Fogaça <yagofogaca20@gmail.com>
2024-03-02 09:26:12 -0300

    style: Estilo global parcial

---
+++ src/global.styled.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+:root {
+  font-size: 16px;
+  font-family: "Open Sans", sans-serif;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+}`;
