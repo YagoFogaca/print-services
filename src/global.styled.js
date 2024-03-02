db60476eb229988c9a465cf97cda9d56fdb3f67f
Yago Fogaça <yagofogaca20@gmail.com>
2024-03-02 09:43:30 -0300

    style fix: correção da estilização padrão

--- src/global.styled.js
+++ src/global.styled.js
@@ -4,9 +4,10 @@ export const GlobalStyled = createGlobalStyle`
 :root {
   font-size: 16px;
   font-family: "Open Sans", sans-serif;
+}
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
-}`;
+`;
