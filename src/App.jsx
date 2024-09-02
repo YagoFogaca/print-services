7ec610163c5ac0f59c9b5287da89d2176458b904
Yago Fogaça <yagofogaca20@gmail.com>
2024-09-02 19:18:43 -0300

    feat: alteração para bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-import { Menu } from "./components/menu/index.menu";
-import { Header } from "./components/header/index.header";
-
 export function App() {
-  return (
-    <>
-      <Header>
-        <Menu />
-      </Header>
-    </>
-  );
+  return <></>;
 }
